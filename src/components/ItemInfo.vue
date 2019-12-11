60df5e413b1b78cd81ca62c241efc2177c9b2a1a
Matthias <Matthias@DESKTOP-K1EQ9VO>
2019-12-11 20:53:41 +0100

    clicked button is now marked (timespan-buttons)

--- src/components/ItemInfo.vue
+++ src/components/ItemInfo.vue
@@ -4,17 +4,12 @@
     <ion-item>
       <ion-grid>
         <ion-row>
-          <ion-col width-25>
-            <ion-button color="tertiary" expand="block">1 Day</ion-button>
-          </ion-col>
-          <ion-col width-25>
-            <ion-button color="tertiary" expand="block">1 Week</ion-button>
-          </ion-col>
-          <ion-col width-25>
-            <ion-button color="tertiary" expand="block">1 Month</ion-button>
-          </ion-col>
-          <ion-col width-25>
-            <ion-button color="tertiary" expand="block">All</ion-button>
+          <ion-col width-25 v-for="button in buttons" v-bind:key="button.id">
+            <ion-button
+              v-bind:color="button.color"
+              expand="block"
+              @click="onTimespanButton_clicked($event,button.id)"
+            >{{button.text}}</ion-button>
           </ion-col>
         </ion-row>
       </ion-grid>
@@ -35,16 +30,41 @@
 </template>
 
 <script>
-import ItemPriceGraph from './ItemPriceGraph.vue';
+const COLOR_CLICKED_BUTTON = "primary";
+const COLOR_UNCLICKED_BUTTON = "tertiary";
+
+import ItemPriceGraph from "./ItemPriceGraph.vue";
 export default {
   name: "item-info",
   components: {
     ItemPriceGraph
   },
+  data() {
+    return {
+      buttons: [
+        { id: 1, color: COLOR_CLICKED_BUTTON, text: "1 Day", isClicked = true, timespan = 1000*60*60*24},
+        { id: 2, color: COLOR_UNCLICKED_BUTTON, text: "1 Week", isClicked = false, timespan = 1000*60*60*24*7},
+        // timespan of one month: current time - current time (1 month subtracted)
+        { id: 3, color: COLOR_UNCLICKED_BUTTON, text: "1 Month", isClicked = false, timespan = (new Date().getTime() - new Date().setMonth(new Date().getMonth()-1).getTime())},
+        { id: 4, color: COLOR_UNCLICKED_BUTTON, text: "All Time", isClicked = false, timespan = new Date().getTime()}
+      ]
+    };
+  },
   mounted() {},
   methods: {
     switchView() {
       this.$emit("switch-view");
+    },
+    onTimespanButton_clicked(e, idClickedButton) {
+      this.buttons.forEach(button => {
+        if (button.id != idClickedButton) {
+          button.color = COLOR_UNCLICKED_BUTTON;
+          button.isClicked = false;
+        } else {
+          button.color = COLOR_CLICKED_BUTTON;
+          button.isClicked = true;
+        }
+      });
     }
   }
 };
